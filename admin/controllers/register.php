ac5136febb47d3bba7a23001b80d8c24eb658227
Justine Stroup <jjstroup@mail.fhsu.edu>
2021-04-01 21:29:11 -0600

    Fourth Commit

--- admin/controllers/register.php
+++ admin/controllers/register.php
@@ -2,11 +2,13 @@
     switch($action) {
         case 'register':
             // Check if user has registered firstname, if not, send to form
-            if (!isset($_POST["fname"]) && !isset($firstname)) {
+            if (!isset($_POST["fname"]) && !isset($firstname) &&!isset($_SESSION["userid"])) {
             include('view/register.php'); 
             } else {
                 $firstname = $_POST["fname"];
-                echo 'Thank you for registering, ' . $firstname . '!';
-            }
+                $_SESSION["userid"] = $firstname;
+                echo 'Thank you for registering, ' . $firstname . '!'; ?>
+                <p><a href=".?action=list_vehicles">Return To Vehicles</a></p>
+            <?php }
             break;
     }
